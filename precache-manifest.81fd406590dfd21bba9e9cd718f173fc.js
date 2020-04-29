1969d875997ef3d10c6e341bd8c83282fa59d65c
dkupshm <dkupsh@ucsb.edu>
2020-04-29 14:45:45 -0700

    Updates

---
+++ precache-manifest.81fd406590dfd21bba9e9cd718f173fc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2992ae5e8c8622508f118e4fe48f4bb4",
+    "url": "/CS_185_React/index.html"
+  },
+  {
+    "revision": "946a64b61f681530364a",
+    "url": "/CS_185_React/static/css/main.21d09ebf.chunk.css"
+  },
+  {
+    "revision": "409bcf4c392cfb6083da",
+    "url": "/CS_185_React/static/js/2.c3ffb78c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CS_185_React/static/js/2.c3ffb78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946a64b61f681530364a",
+    "url": "/CS_185_React/static/js/main.b2e728c4.chunk.js"
+  },
+  {
+    "revision": "7b16177473e282f7fec3",
+    "url": "/CS_185_React/static/js/runtime-main.8f8664c3.js"
+  },
+  {
+    "revision": "7168856bdc252612926d01b9005d6a72",
+    "url": "/CS_185_React/static/media/dog5.7168856b.jpeg"
+  },
+  {
+    "revision": "ef05f033129a2033ff5860998b964bff",
+    "url": "/CS_185_React/static/media/dog6.ef05f033.jpeg"
+  },
+  {
+    "revision": "8e44e4c751d91753514eaf603f9aca3e",
+    "url": "/CS_185_React/static/media/ucd.8e44e4c7.jpeg"
+  },
+  {
+    "revision": "ad9042a1f9e4773e846d464325b3fc46",
+    "url": "/CS_185_React/static/media/ucsb.ad9042a1.jpeg"
+  },
+  {
+    "revision": "7bf3f6079480e37517d4a77cdab75be5",
+    "url": "/CS_185_React/static/media/ucsc.7bf3f607.jpeg"
+  },
+  {
+    "revision": "0d8a55aaed35a8b67d4efe68d2242d04",
+    "url": "/CS_185_React/static/media/ucsd.0d8a55aa.jpeg"
+  }
+]);
